b110b012a757e10770b3307a8836b59ce611dc34
aavasantala <aavasantala1@gmail.com>
2024-04-05 13:26:21 +0300

    NYT

--- Sivut/Android/app/src/main/java/com/example/elokuvatietokanta/MainActivity.kt
+++ Sivut/Android/app/src/main/java/com/example/elokuvatietokanta/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.elokuvatietokanta
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+
+    var vittu=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
@@ -26,18 +30,58 @@ class MainActivity : ComponentActivity() {
         val choosesignUpButton = findViewById<Button>(R.id.signUpButton)
         val continueButton = findViewById<Button>(R.id.continueButton)
 
+
+
+        val username=findViewById<EditText>(R.id.usernameText)
+        val password=findViewById<EditText>(R.id.passwordText)
+        val email=findViewById<EditText>(R.id.emailText)
+
+
+
         chooseLoginButton.setOnClickListener {
+
+
+            vittu=1
             setContentView(R.layout.login_screen)
             loginButtons()
         }
 
         choosesignUpButton.setOnClickListener {
+
+            vittu=2
             setContentView(R.layout.signup_screen)
             loginButtons()
+
         }
 
         continueButton.setOnClickListener {
-            setContentView(R.layout.addorsearch)
+
+            if(vittu==1){
+                if(username.text.isNotEmpty() && password.text.isNotEmpty()){
+                    setContentView(R.layout.addorsearch)
+                    menuButtons()
+                }
+                else{
+
+                    Toast.makeText(this, "Täytä kaikki kohdat", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+
+            else{
+                if(username.text.isNotEmpty() && password.text.isNotEmpty() && email.text.isNotEmpty()){
+                    setContentView(R.layout.addorsearch)
+                    menuButtons()
+                }
+                else{
+
+                    Toast.makeText(this, "Täytä kaikki kohdat", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+
         }
     }
 }
