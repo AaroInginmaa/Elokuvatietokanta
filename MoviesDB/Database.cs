d6773a8ead0c655ab2b2bb97e4303fc76ca25dfb
v1to <pixel2006.aa@gmail.com>
2024-04-11 02:16:37 +0300

    Initial Blazor ASP.NET project

---
+++ MoviesDB/Database.cs
@@ -0,0 +1,95 @@
+﻿using MoviesDB.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MoviesDB
+{
+    public class Database
+    {
+        private readonly string _host;
+        private readonly string _database;
+        private readonly string _user;
+        private readonly string _password;
+        private readonly int _port;
+
+        private MySqlConnection _connection;
+
+        public Database()
+        {
+            _host = "localhost";
+            _database = "movies_db";
+            _user = "admin";
+            _password = "admin";
+            _port = 3306;
+
+            string connectionQuery = $"Server={_host};Port={_port};Database={_database};Uid={_user};Pwd={_password};";
+
+            _connection = new MySqlConnection(connectionQuery);
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+
+            string query = "SELECT * FROM movies_db.movies;";
+
+            OpenConnection();
+            MySqlCommand mySqlCommand = new(query, _connection);
+
+            MySqlDataAdapter mySqlDataAdapter = new(mySqlCommand);
+
+            DataTable DataTable = new();
+
+            mySqlDataAdapter.Fill(DataTable);
+            CloseConnection();
+
+            foreach (DataRow dataRow in DataTable.Rows)
+            {
+                movies.Add(new Movie
+                    (
+                    Convert.ToString(dataRow["title"]),
+                    Convert.ToInt32(dataRow["movie_id"]),
+                    Convert.ToString(dataRow["director"]),
+                    Convert.ToInt32(dataRow["release_year"]),
+                    Convert.ToInt32(dataRow["length"]),
+                    Convert.ToInt32(dataRow["rating"]),
+                    Convert.ToString(dataRow["genre"]),
+                    Convert.ToString(dataRow["image"])
+                    ));
+            }
+
+            return movies;
+        }
+
+        public void InsertMovie(Movie movie)
+        {
+            string query = $"INSERT INTO movies_db.movies VALUES (" +
+                $"{movie.Id}," +
+                $"\"{movie.Title}\"," +
+                $"\"{movie.Director}\"," +
+                $"{movie.ReleaseYear}," +
+                $"{movie.Length}," +
+                $"{movie.Rating}," +
+                $"\"{movie.Genre}\"," +
+                $"\"{movie.Image}\"" +
+                $");";
+
+            OpenConnection();
+
+            MySqlCommand mySqlCommand = new(query, _connection);
+            MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+            CloseConnection();
+        }
+
+        private void OpenConnection()
+        {
+            _connection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            _connection.Close();
+        }
+    }
+}
---
+++ MoviesDB/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace MoviesDB.Models
+{
+    public class Movie
+    {
+        public Movie(string title, int id = 0, string director = "", int releaseYear = 0, int length = 0, int rating = 0, string genre = "", string image = "")
+        {
+            Title = title;
+            Id = id;
+            Director = director;
+            ReleaseYear = releaseYear;
+            Length = length;
+            Rating = rating;
+            Genre = genre;
+            Image = image;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Length { get; set; }
+        public int Rating { get; set; }
+        public string Genre { get; set; }
+        public string Image { get; set; }
+    }
+}
