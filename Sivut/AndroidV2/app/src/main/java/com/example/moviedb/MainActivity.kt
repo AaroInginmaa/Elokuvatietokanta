3b13ab4138cfd37570a31fd78aad322054be16f9
EinoTaimisto <eino.taimisto@outlook.com>
2024-04-18 08:40:20 +0300

    Projekti uusiks githubiin
    Kokeillaan uusiks android

---
+++ Sivut/AndroidV2/app/src/main/java/com/example/moviedb/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.moviedb
+
+//import android.os.your.mother
+//import vittuku.tätä.ei.oo.dokumentoitu.eikä.tehty.eteepäi.vaan.todettu.että.tää.on.seuraavan.ongelma...
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    //tällä katotaan että ollaanko login vai rekisteröinti ruudussa.
+    private var isPressed = 1
+
+    private var usernameValue: String = ""
+    private var passwordValue: String = ""
+
+    //kun ohjelma alkaa, mennään loginscreeniin
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+        loginButtons()
+    }
+
+    //hae ja lisää elokuva ruudun nappulat
+    private fun menuButtons() {
+        val searchButton = findViewById<Button>(R.id.searchButton)
+        val addButton = findViewById<Button>(R.id.addButton)
+
+        searchButton.setOnClickListener {
+            setContentView(R.layout.searchmovie)
+        }
+        addButton.setOnClickListener {
+            setContentView(R.layout.addmovie)
+        }
+    }
+
+    //login screenin napit
+    private fun loginButtons() {
+
+        //login screen tab nappi
+        val chooseLoginButton = findViewById<Button>(R.id.loginButton)
+
+        //rekisteröinti screen tab nappi
+        val choosesignUpButton = findViewById<Button>(R.id.signUpButton)
+
+        //kirjaudu/rekisteröidy nappi alhaalla
+        val continueButton = findViewById<Button>(R.id.continueButton)
+
+        //text fieldit kirjautumis/rekisteröinti ruuduissa
+        val usernameEditText = findViewById<EditText>(R.id.usernameText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordText)
+        val emailEditText = findViewById<EditText>(R.id.emailText)
+
+
+        //jos painaa login tab nappia ylhäällä niin menee login ruutuun
+        chooseLoginButton.setOnClickListener {
+            //tämä settaa sen että ollaan login ruudussa (tiedetään että tarvitaan ottaa vain kahdesta text fieldistä eikä kolmesta
+            isPressed = 1
+            setContentView(R.layout.login_screen)
+            loginButtons()
+        }
+
+        //jos painaa rekisteröidy tab nappia ylhäällä niin menee rekisteröidy ruutuun
+        choosesignUpButton.setOnClickListener {
+            //ollaan rekisteröinti ruudussa, otetaan kaikki kolme text fieldiä
+            isPressed = 2
+            setContentView(R.layout.signup_screen)
+            loginButtons()
+        }
+
+        //login ja rekisteröinti nappi alhaalla joka checkaa onko kaikki kohdat täytetty ja onko salasana valid
+        continueButton.setOnClickListener {
+            if (isPressed == 1 || isPressed == 2) {
+                if (usernameEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty() && (isPressed == 1 || emailEditText.text.isNotEmpty())) {
+                    val passwordStr = passwordEditText.text.toString()
+                    if (isPasswordValid(passwordStr)) {
+                        usernameValue = usernameEditText.text.toString()
+                        passwordValue = passwordStr
+
+                        if(checkLogin(usernameValue, passwordValue)){
+                            setContentView(R.layout.addorsearch)
+                            menuButtons()
+                        }
+                        else{
+                            Toast.makeText(this, "Käyttäjänimi, sähköposti, tai salasana oli väärin", Toast.LENGTH_SHORT).show()
+                        }
+
+                    } else {
+                        Toast.makeText(this, "Salasana ei ole tarpeeksi vahva", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Täytä kaikki kohdat", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    //onko password tarpeeksi vahva
+    private fun isPasswordValid(password: String): Boolean {
+        if (password.length < 6) {
+            return false
+        }
+
+        val containsUpperCase = password.any { it.isUpperCase() }
+        if (!containsUpperCase) {
+            return false
+        }
+
+        val specialCharacters = setOf('!', '#', '?', '&', '%', '$', '€', '£', '@')
+        return password.any { specialCharacters.contains(it) }
+    }
+
+    private fun checkLogin(username:String, password: String): Boolean{
+        //katso onko databasesta username ja salasana oikeat, tätä pitää muuttaa sitten siten että se oikeasti
+        //katsoo sieltä databasesta nämä
+        if (username==username&&password==password){
+            return true
+        }
+        return false
+    }
+
+}
