e931e9417f366bfa6c566aec385cf6eed8973329
Aaro Inginmaa <77359720+AaroInginmaa@users.noreply.github.com>
2024-04-26 13:28:35 +0300

    Hommia

--- Sivut/JetpackAndroid/app/build.gradle.kts
+++ Sivut/JetpackAndroid/app/build.gradle.kts
@@ -59,6 +59,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    /*
+    * Lataa mysql connector https://dev.mysql.com/downloads/connector/j/
+    * Valitse sivulta platform independent valinta ja lataa .zip filussa
+    * kopioi tiedosto mysql-connector-j-8.3.0.jar -> c:/joku/helppo/paikka/
+    * ja laita alla olevaksi tiedosto sijainniksi se mihin kopioit .jar tiedoston
+    */
+    implementation(files("C:\\java\\mysql-connector.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +73,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
