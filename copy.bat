3a68e1d2cb29f6197f45f6883fe211a037916d53
Aaro Inginmaa <77359720+AaroInginmaa@users.noreply.github.com>
2024-05-16 10:40:41 +0300

    Fixattu

--- copy.bat
+++ copy.bat
@@ -1,7 +1,7 @@
 @echo Poistetaan vanhoja tiedostoja . . .
-@echo rmdir /s/q C:\xampp\htdocs\php
+rmdir /s/q C:\xampp\htdocs\php
 
 @echo Kopioidaan tiedostoja . . .
-@echo xcopy /s /I c:\path\to\Elokuvatietokanta\php\ c:\path\to\xampp\htdocs\php
+xcopy /s /I /Y c:\repos\Elokuvatietokanta\php C:\xampp\htdocs\php
 
 @echo Valmis
