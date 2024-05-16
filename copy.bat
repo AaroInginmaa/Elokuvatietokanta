af467d0cb60e13db77f323736b18b7f4a11ea9d1
Aaro Inginmaa <77359720+AaroInginmaa@users.noreply.github.com>
2024-05-16 08:58:07 +0300

    Scripti joka päivittää tiedostot

---
+++ copy.bat
@@ -0,0 +1,7 @@
+@echo Poistetaan vanhoja tiedostoja . . .
+@echo rmdir /s/q C:\xampp\htdocs\php
+
+@echo Kopioidaan tiedostoja . . .
+@echo xcopy /s /I c:\path\to\Elokuvatietokanta\php\ c:\path\to\xampp\htdocs\php
+
+@echo Valmis
